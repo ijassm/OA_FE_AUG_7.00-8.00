ffc709d5392ffea59cfbf127c36f268c3ca77907
“ijassm” <ijass.ocean@gmail.com>
2023-11-15 20:07:26 +0530

    event -- 15 Nov 2023

---
+++ JS/DOM2/index.js
@@ -0,0 +1,48 @@
+// add event listener
+// const doc = document;
+// const button = doc.querySelector("button");
+
+// button.addEventListener('click', () => console.log("hello"));
+// button.addEventListener('click', () => console.log("hello"));
+// button.addEventListener('click', () => console.log("hello"));
+
+
+// function submit() {
+//     const email = doc.querySelector(`input[type="email"]`).value;
+//     const password = doc.querySelector(`input[type="password"]`).value;
+
+//     // console.log(email);
+//     // console.log(password);
+
+//     // const obj = {
+//     //     email: email,
+//     //     password: password,
+//     // }
+
+//     console.log(email);
+//     console.log(password);
+
+// }
+
+// let i = 0
+// function inc() {
+//     console.log(++i);
+// }
+
+// web api(window)
+// setTimeout
+// setInterval
+
+// setTimeout(() => {
+//     console.log("hello you called me");
+// }, 1000)
+
+// let c = 0;
+// setInterval(() => {
+//     console.log("setinterval called me", ++c);
+// }, 2000)
+
+
+// for (let i = 0; i < 50000; i++) {
+//     console.log("hello am outside the func", i);
+// }
